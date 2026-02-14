562f0c3aab9b576918cbda9b372d620003a96f7a
WardanaH <rizkiwardana10@gmail.com>
2026-02-14 09:10:27 +0700

    updates

--- app/Events/NotifikasiSpkLembur.php
+++ app/Events/NotifikasiSpkLembur.php
@@ -18,22 +18,20 @@ class NotifikasiSpkLembur implements ShouldBroadcastNow
     public $pesan;
     public $tipe_spk;
     public $no_spk;
-    public $cabang_id;
 
 
-    public function __construct($no_spk, $tipe_spk, $cabang_id, $designer_nama)
+    public function __construct($no_spk, $tipe_spk, $designer_nama)
     {
         $this->no_spk = $no_spk;
         $this->tipe_spk = $tipe_spk;
         $this->pesan = "SPK {$tipe_spk} Baru oleh {$designer_nama}!";
-        $this->cabang_id = $cabang_id;
 
         Log::info("EVENT CONSTRUCT: Notifikasi dibuat untuk SPK Lembur no - " . $no_spk);
     }
     public function broadcastOn()
     {
         Log::info("EVENT BROADCAST: Mengirim ke channel-admin");
-        return new Channel('channel-lembur-' . $this->cabang_id);
+        return new Channel('channel-lembur');
     }
 
     public function broadcastAs()
